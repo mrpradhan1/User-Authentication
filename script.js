750bbfd3426078a7afbe5befa5b0808c2be36309
Mano <manoranjanpradhan586@gmail.com>
2023-09-23 21:55:16 +0530

    first

---
+++ script.js
@@ -0,0 +1,66 @@
+// Get references to HTML elements
+const signupForm = document.getElementById("signup-form");
+const usernameInput = document.getElementById("username");
+const emailInput = document.getElementById('emailID');
+const passwordInput = document.getElementById("password");
+const confPasswordInput = document.getElementById("conf-psw");
+const signupMessage = document.getElementById("signup-message");
+const profileDetails = document.getElementById("profile-details");
+const logoutButton = document.getElementById("logout-btn");
+
+// Check if the user is already logged in (has an access token)
+const accessToken = localStorage.getItem("access_token");
+
+if (accessToken) {
+    // User is logged in, redirect to the profile page
+    window.location.href = "profile.html";
+}
+
+// Handle Signup Form Submission
+signupForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Get user input
+    const username = usernameInput.value;
+    const emailID= emailInput.value;
+    const password = passwordInput.value;
+
+    // Validate input (add more validation as needed)
+    if (!username || !password) {
+        signupMessage.textContent = "Please fill in all fields.";
+        return;
+    }
+
+    // Generate a random 16-byte access token (hexadecimal)
+    const accessToken = Array.from({ length: 16 }, () =>
+        Math.floor(Math.random() * 256).toString(16).padStart(2, "0")
+    ).join("");
+
+    // Store user data in local storage
+    localStorage.setItem("username", username);
+    localStorage.setItem("access_token", accessToken);
+
+    // Display success message and redirect to the profile page
+    signupMessage.textContent = "Signup successful!";
+    setTimeout(() => {
+        window.location.href = "profile.html";
+    }, 1000);
+});
+
+// Check if the user is logged in on the profile page
+if (!accessToken) {
+    // User is not logged in, redirect to the signup page
+    window.location.href = "index.html";
+} else {
+    // Display user details on the profile page
+    const username = localStorage.getItem("username");
+    profileDetails.innerHTML = `<p>Username: ${username}</p>`;
+}
+
+// Handle Logout
+logoutButton.addEventListener("click", function () {
+    // Clear local storage and redirect to the signup page
+    localStorage.removeItem("username");
+    localStorage.removeItem("access_token");
+    window.location.href = "index.html";
+});
